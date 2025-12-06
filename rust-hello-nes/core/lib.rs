8678f176209b2dffb5eff3871d6ebe24f8c19095
statiolake <statiolake@gmail.com>
2025-12-06 13:51:52 +0000

    feat: add missing functions to core

--- rust-hello-nes/core/lib.rs
+++ rust-hello-nes/core/lib.rs
@@ -215,3 +215,45 @@ mod iter_private;
 mod nonzero;
 mod tuple;
 mod unit;
+
+#[alloc_error_handler]
+
+fn alloc_error_handler(_layout: ()) -> ! {
+    loop {}
+}
+
+#[panic_implementation]
+
+pub fn panic_impl(_info: ()) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+
+extern "C" fn __rdl_alloc() {
+    loop {}
+}
+
+#[no_mangle]
+
+extern "C" fn __rdl_alloc_zeroed() {
+    loop {}
+}
+
+#[no_mangle]
+
+extern "C" fn __rdl_dealloc() {
+    loop {}
+}
+
+#[no_mangle]
+
+extern "C" fn __rdl_realloc() {
+    loop {}
+}
+
+#[no_mangle]
+
+extern "C" fn _Unwind_Resume() {
+    loop {}
+}
